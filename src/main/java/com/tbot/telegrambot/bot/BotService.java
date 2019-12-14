664264a2ddffe450d0d692669afbb6c654731337
ishvatov <ishvatov@gmail.com>
2019-12-15 00:11:19 +0300

    Changed scheduler time

--- src/main/java/com/tbot/telegrambot/bot/BotService.java
+++ src/main/java/com/tbot/telegrambot/bot/BotService.java
@@ -130,7 +130,7 @@ public class BotService extends TelegramLongPollingBot {
         }
     }
 
-    @Scheduled(cron = "10 0 * * * ?")
+    @Scheduled(cron = "0 15 0 * * ?")
     public void notifyUsers() {
         for (Map.Entry<Long, String> user : currentUsers.entrySet()) {
             Set<TodoEntity> tasksForToday = findTasksForToday(
